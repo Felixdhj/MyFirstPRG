99f6d041bedc4ddcc69c9d2b895429de8818b2af
felix <felix1942@gmail.com>
2025-08-15 13:09:58 +0900

    Add project files.

---
+++ MyFirstPRG/MyFirstPRGDlg.h
@@ -0,0 +1,34 @@
+
+// MyFirstPRGDlg.h : header file
+//
+
+#pragma once
+
+
+// CMyFirstPRGDlg dialog
+class CMyFirstPRGDlg : public CDialogEx
+{
+// Construction
+public:
+	CMyFirstPRGDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYFIRSTPRG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
